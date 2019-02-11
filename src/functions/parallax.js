e7183dd95ae90becad7088a4f7676a16629530b6
Rinat R <evolticdev@gmail.com>
2019-02-11 21:29:05 +0500

    Change the root to the dist folder files

--- src/functions/parallax.js
+++
@@ -1,8 +0,0 @@
-function parallax(intensity, element) {
-	window.addEventListener('scroll', () => {
-		const property = window.pageYOffset / intensity
-		element.style.transform = `matrix(1, 0, 0, 1, 0, ${property})`
-	})
-}
-
-export default parallax
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import parallax from './functions/parallax'
-
-parallax(2, document.querySelector('.section_preface'))
